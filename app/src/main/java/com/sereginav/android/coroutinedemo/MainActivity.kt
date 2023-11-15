a55fefbbe969a45eb0cf9f1ec52ee6d62ea7abd5
LooperMind <sear_ring@mail.ru>
2023-11-15 19:45:25 +0700

    added suspend fun

--- app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import com.sereginav.android.coroutinedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding;
     private var count: Int = 1
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    suspend fun performTask(taskNumber : Int) : Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5000)
+            return@async "FinishedCoroutine $taskNumber"
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 count = p1
-                binding.countText.text = "${count} coroutines"
+                binding.countText.text = "$count coroutines"
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
