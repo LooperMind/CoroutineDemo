2764397f7052910b8ba8d8013e0e7e80de602b27
LooperMind <sear_ring@mail.ru>
2023-11-15 19:41:17 +0700

    added seekBar valChanged Listener

--- app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.sereginav.android.coroutinedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.sereginav.android.coroutinedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding;
+    private var count: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                count = p1
+                binding.countText.text = "${count} coroutines"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+            }
+        )
     }
 }
