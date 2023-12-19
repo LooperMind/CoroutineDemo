f07b20065d84b36ae5616629794d9115172825ab
LooperMind <40730879+LooperMind@users.noreply.github.com>
2023-12-19 21:45:14 +0700

    Убрано обращение через binding

--- app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/sereginav/android/coroutinedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.SeekBar
+import android.widget.TextView
 import com.sereginav.android.coroutinedemo.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
@@ -11,10 +12,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private var count: Int = 1
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var seekBar: SeekBar
+    private lateinit var statusText : TextView
 
     suspend fun performTask(taskNumber : Int) : Deferred<String> =
         coroutineScope.async(Dispatchers.Main) {
-            delay(5000)
+            delay(5_000)
             return@async "FinishedCoroutine $taskNumber"
         }
 
@@ -22,12 +25,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-
-        binding.seekBar.setOnSeekBarChangeListener(object :
+        seekBar = findViewById(R.id.seekBar)
+        statusText = findViewById(R.id.statusText)
+        seekBar.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                count = p1
-                binding.countText.text = "$count coroutines"
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                count = progress
+                statusText.text = "$count coroutines"
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -43,9 +47,9 @@ class MainActivity : AppCompatActivity() {
 
     fun launchCoroutines(view : View){
         (1..count).forEach{
-            binding.statusText.text = "Started coroutine $it"
+            statusText.text = "Started coroutine $it"
             coroutineScope.launch(Dispatchers.Main){
-                binding.statusText.text = performTask(it).await()
+                statusText.text = performTask(it).await()
             }
         }
     }
